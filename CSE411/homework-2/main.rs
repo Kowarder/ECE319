99620ecabc3cf9de31e3839666c008827f756b1d
Kowarder <70952974+Kowarder@users.noreply.github.com>
2023-12-25 23:24:14 -0500

    Add files via upload

--- CSE411/homework-2/main.rs
+++ CSE411/homework-2/main.rs
@@ -1,132 +1,169 @@
-// #![feature(test)]
-use std::io::{self, prelude::*, BufReader, Write};
-use std::net::TcpStream;
-use std::{str, thread};
-// extern crate test;
-// use std::time::{Instant};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::thread;
+use std::io;
+extern crate lazy_static;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use std::sync::mpsc;
+use std::sync::Arc;
+extern crate threadpool;
+use threadpool::ThreadPool; 
 
-// fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
-//     loop {
-//         println!("Please input the request:");
-//         let mut input_request = String::new();
-//         io::stdin().read_line(&mut input_request).expect("failed to read");
-//         if input_request == "put\n" {
-//             println!("Please enter the key-len:");
-//             let mut input_kl = String::new();
-//             io::stdin().read_line(&mut input_kl).expect("failed to read");
-//             println!("Please enter the key:");
-//             let mut input_key = String::new();
-//             io::stdin().read_line(&mut input_key).expect("failed to read");
-//             println!("Please enter the value-len:");
-//             let mut input_vl = String::new();
-//             io::stdin().read_line(&mut input_vl).expect("failed to read");
-//             println!("Please enter the value:");
-//             let mut input_value = String::new();
-//             io::stdin().read_line(&mut input_value).expect("failed to read");
-//             let mut input = String::new();
-//             input = "PUT\n".to_owned() + &input_kl + &input_key + &input_vl + &input_value;
+lazy_static! {
+    static ref global_vec: Mutex<Vec<node>> = Mutex::new(Vec::new());
+}
+struct node {
+    key_len: String,
+    key: String,
+    value_len: String,
+    value: String
+}
 
-//             stream.write(input.as_bytes()).expect("fail to write");
-//         } else if input_request == "get\n" {
-//             println!("Please enter the key-len:");
-//             let mut input_kl = String::new();
-//             io::stdin().read_line(&mut input_kl).expect("failed to read");
-//             println!("Please enter the key:");
-//             let mut input_key = String::new();
-//             io::stdin().read_line(&mut input_key).expect("failed to read");
-//             let mut input = String::new();
-//             input = "GET\n".to_owned() + &input_kl + &input_key;
-
-//             stream.write(input.as_bytes()).expect("fail to write");
-//         } else if input_request == "del\n" {
-//             println!("Please enter the key-len:");
-//             let mut input_kl = String::new();
-//             io::stdin().read_line(&mut input_kl).expect("failed to read");
-//             println!("Please enter the key:");
-//             let mut input_key = String::new();
-//             io::stdin().read_line(&mut input_key).expect("failed to read");
-//             let mut input = String::new();
-//             input = "DEL\n".to_owned() + &input_kl + &input_key;
-
-//             stream.write(input.as_bytes()).expect("fail to write");
-//         } else {
-//             break
-//         }
-//         let mut reader = BufReader::new(&stream);
-//         let mut buffer: Vec<u8> = Vec::new();
-//         reader.read_until(b'\r', &mut buffer).expect("fail to read into buffer");
-//         println!("{}", str::from_utf8(&buffer).unwrap());
-//     };
-//     Ok(())
+// struct ThreadPool {
+//     workers: Vec<Worker>,
+//     sender: mpsc::Sender<Job>,
 // }
 
+// impl ThreadPool {
+//     fn new(size: usize) -> ThreadPool {
+//         assert!(size > 0);
+//         let (sender, receiver) = mpsc::channel();
+//         let receiver = Arc::new(Mutex::new(receiver));
+//         let mut workers = Vec::with_capacity(size);
+//         for id in 0..size {
+//             workers.push(Worker::new(id, Arc::clone(&receiver)));
+//         }
+//         ThreadPool { workers, sender }
+//     }
+
+//     fn execute<F>(&self, f:F) 
+//     where
+//         F:FnOnce() + Send + 'static,
+//         {
+//             let job = Box::new(f);
+//             self.sender.send(job).unwrap();
+//         }
+// }
+
+// struct Worker {
+//     id: usize,
+//     thread: thread::JoinHandle<()>,
+// }
+// impl Worker {
+//     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+//         let thread = thread::spawn(move || loop {
+//             while let Ok(job) = receiver.lock().unwrap().recv(){
+//                 job.call_box();
+//             }
+//         });
+//         Worker { id, thread }
+//     }
+    
+// }
+// type Job = Box<dyn FnBox + Send + 'static>;
+// trait FnBox {
+//     fn call_box(self: Box<Self>);
+// }
+// impl <F: FnOnce()>FnBox for F {
+//     fn call_box(self:Box<F>) {
+//         (*self)()
+//     }
+// }
 
 fn main() -> io::Result<()>{
-    let mut stream = TcpStream::connect("127.0.0.1:1895").unwrap();
+    
+    let listener = TcpListener::bind("127.0.0.1:1895").unwrap();
     // let mut thread_vec : Vec<thread::JoinHandle<()>> = Vec::new();
 
-    // thread::spawn(move || {
-    //     println!("come in");
-    //     handle_connection(stream);
+    // for stream in listener.incoming() {
+    //     let _stream = stream.unwrap();
+        
+    //     let handle = thread::spawn(move || {
+    //         handle_connection(_stream).unwrap_or_else(|error| eprintln!("{:?}", error));
     //     });
+    //     thread_vec.push(handle);
+    // }
 
     // for handle in thread_vec {
-    //         handle.join().unwrap();
-    //     }
-    loop {
-        // let start_time = Instant::now();
-        println!("Please input the request:");
-        let mut input_request = String::new();
-        io::stdin().read_line(&mut input_request).expect("failed to read");
-        if input_request == "put\n" {
-            println!("Please enter the key-len:");
-            let mut input_kl = String::new();
-            io::stdin().read_line(&mut input_kl).expect("failed to read");
-            println!("Please enter the key:");
-            let mut input_key = String::new();
-            io::stdin().read_line(&mut input_key).expect("failed to read");
-            println!("Please enter the value-len:");
-            let mut input_vl = String::new();
-            io::stdin().read_line(&mut input_vl).expect("failed to read");
-            println!("Please enter the value:");
-            let mut input_value = String::new();
-            io::stdin().read_line(&mut input_value).expect("failed to read");
-            let mut input = String::new();
-            input = "PUT\n".to_owned() + &input_kl + &input_key + &input_vl + &input_value;
+    //     handle.join().unwrap();
+    // }
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+    Ok(())
+}
 
-            stream.write(input.as_bytes()).expect("fail to write");
-        } else if input_request == "get\n" {
-            println!("Please enter the key-len:");
-            let mut input_kl = String::new();
-            io::stdin().read_line(&mut input_kl).expect("failed to read");
-            println!("Please enter the key:");
-            let mut input_key = String::new();
-            io::stdin().read_line(&mut input_key).expect("failed to read");
-            let mut input = String::new();
-            input = "GET\n".to_owned() + &input_kl + &input_key;
+fn handle_connection(mut stream: TcpStream) -> io::Result<()>  {
+    let mut buffer = [0; 512];
+    let mut store = global_vec.lock().unwrap();
 
-            stream.write(input.as_bytes()).expect("fail to write");
-        } else if input_request == "del\n" {
-            println!("Please enter the key-len:");
-            let mut input_kl = String::new();
-            io::stdin().read_line(&mut input_kl).expect("failed to read");
-            println!("Please enter the key:");
-            let mut input_key = String::new();
-            io::stdin().read_line(&mut input_key).expect("failed to read");
-            let mut input = String::new();
-            input = "DEL\n".to_owned() + &input_kl + &input_key;
-
-            stream.write(input.as_bytes()).expect("fail to write");
-        } else {
-            break
+    for _ in 0..1000 {
+        let bytes = stream.read(&mut buffer).unwrap();
+        if bytes == 0 {
+          return Ok(());  
         }
-        let mut reader = BufReader::new(&stream);
-        let mut buffer: Vec<u8> = Vec::new();
-        reader.read_until(b'\r', &mut buffer).expect("fail to read into buffer");
-        println!("{}", str::from_utf8(&buffer).unwrap());
-        // let elapsed_time = start_time.elapsed();
-        // println!("{:?}", elapsed_time);
-    };
+        let data_string = String::from_utf8_lossy(&buffer);
+        // println!("{}", data_string);
+
+        let mut parts = data_string.split("\n");
+        let part1 = parts.next().unwrap_or_default();
+        let mut flag = 0;
+        if part1 == "PUT" {
+            println!("Now is put");
+            let part2 = parts.next().unwrap_or_default();
+            let part3 = parts.next().unwrap_or_default();
+            let part4 = parts.next().unwrap_or_default();
+            let part5 = parts.next().unwrap_or_default();
+
+            let index = store.iter().position(|n| n.key == part3);
+            if let Some(index) = index {
+                store.drain(index..index+1);
+            }
         
+            let Node_temp = node {
+                key_len: part2.to_string(),
+                key: part3.to_string(),
+                value_len: part4.to_string(),
+                value: part5.to_string(),
+            };
+            store.push(Node_temp);
+            println!("already push");
+            stream.write(b"OK\n\r").unwrap();
+
+        } else if part1 == "GET" {
+            println!("Now is get");
+            let part2 = parts.next().unwrap_or_default();
+            let part3 = parts.next().unwrap_or_default();
+            let mut flag = 0;
+            for item in store.iter() {
+                if item.key == part3 {
+                    let mut rst = String::new();
+                    rst = ("OK\n").to_string() + &item.value_len + "\n" + &item.value + "\n\r";
+                    stream.write(rst.as_bytes()).unwrap();
+                    flag = 1;
+                }
+            }
+            if flag == 0 {
+                stream.write(b"ERROR\n\r").unwrap();
+            }
+        } else if part1 == "DEL" {
+            println!("Now is del");
+            let part2 = parts.next().unwrap_or_default();
+            let part3 = parts.next().unwrap_or_default();
+            let index = store.iter().position(|n| n.key == part3);
+            if let Some(index) = index {
+                store.drain(index..index+1);
+                stream.write(b"OK\n\r").unwrap();
+            } else {
+                stream.write(b"ERROR\n\r").unwrap();
+            }
+        }
+        // stream.write(&buffer[..bytes]).unwrap();
+    }
+    
     Ok(())
 }
