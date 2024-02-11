ebaaf5db6a7347ecd3bce2853115d42b3bd31fd7
Kowarder <70952974+Kowarder@users.noreply.github.com>
2024-02-10 19:41:17 -0500

    Add files via upload

---
+++ ECE403/CNN/temp2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 19:37:25 2024
+
+@author: xuyifeng
+"""
+
+import tensorflow.keras as keras
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import (
+    Dense,
+    Conv2D,
+    MaxPool2D,
+    Flatten,
+    Dropout,
+    BatchNormalization,
+)
+
+# Load in our data from CSV files
+train_df = pd.read_csv("sign_mnist_train.csv")
+valid_df = pd.read_csv("sign_mnist_valid.csv")
+
+# Separate out our target values
+y_train = train_df['label']
+y_valid = valid_df['label']
+del train_df['label']
+del valid_df['label']
+
+# Separate out our image vectors
+x_train = train_df.values
+x_valid = valid_df.values
+
+# Turn our scalar targets into binary categories
+num_classes = 24
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_valid = keras.utils.to_categorical(y_valid, num_classes)
+
+# Normalize our image data
+x_train = x_train / 255
+x_valid = x_valid / 255
+x_train = x_train.reshape(-1,28,28,1)
+x_valid = x_valid.reshape(-1,28,28,1)
+
+
+
+model = Sequential()
+model.add(Conv2D(75, (3, 3), strides=1, padding="same", activation="relu", 
+                 input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Conv2D(50, (3, 3), strides=1, padding="same", activation="relu"))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Conv2D(25, (3, 3), strides=1, padding="same", activation="relu"))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2, 2), strides=2, padding="same"))
+model.add(Flatten())
+model.add(Dense(units=512, activation="relu"))
+model.add(Dropout(0.3))
+model.add(Dense(units=num_classes, activation="softmax"))
+model.summary()
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=20, verbose=1, validation_data=(x_valid, y_valid))
